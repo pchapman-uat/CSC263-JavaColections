f0810abe30965462b9ba5b4e526a430bf49d5a1d
Preston Chapman <pchapman82070@uat.edu>
2024-07-11 15:19:23 -0700

    Add Stack

--- Collections/Stack.java
+++ Collections/Stack.java
@@ -1,4 +1,16 @@
 package Collections;
-public class Stack {
-    
+
+import java.util.LinkedList;
+
+public class Stack<T> {
+    private final LinkedList<T> list = new LinkedList<T>();
+    public void push(T item){
+        list.addFirst(item);
+    }
+    public T pop(){
+        return list.removeFirst();
+    }
+    public T peek(){
+        return list.getFirst();
+    }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
+import Collections.Stack;
+
 class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        Stack<Integer> stack = new Stack<Integer>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        System.out.println(stack.peek());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
     }
 }
